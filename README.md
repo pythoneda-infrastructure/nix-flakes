2c70bf8db5e05dce7d12c6b98e4985c7a5e39404
rydnr <github@acm-sl.org>
2023-05-31 20:06:38 +0200

    Updated flake. Tag 0.0.1a2

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# pythoneda-nix-flakes-infrastructure
-Infrastructure layer for PythonEDA Nix Flakes package
+# Infrastructure layer of PythonEDA Nix Flakes
+
+This package includes the infrastructure layer of [https://github.com/pythoneda/base](PythonEDA) [https://github.com/pythoneda/nix-flakes](Nix Flakes).
+
+This package provides:
+- [PythonEDAInfrastructureNixFlakes/folder_flake_repo.py](PythonEDAInfrastructureNixFlakes/folder_flake_repo.py): A [https://github.com/pythoneda/nix-flakes/PythonEDANixFlakes/flake_repo.py](FlakeRepo) based on a folder.
+- [PythonEDAInfrastructureNixFlakes/recipe/dynamically_discoverable_flake_recipe_repo.py](PythonEDAInfrastructureNixFlakes/recipe/dynamically_discoverable_flake_recipe_repo.py): A dynamically discoverable [https://github.com/pythoneda/nix-flakes/PythonEDANixFlakes/recipe/flake_recipe_repo.py](RecipeRepo).
---
+++ flake.nix
@@ -0,0 +1,81 @@
+{
+  description = "Infrastructure layer of PythonEDA Nix Flakes";
+
+  inputs = rec {
+    nixos.url = "github:NixOS/nixpkgs/nixos-22.11";
+    flake-utils.url = "github:numtide/flake-utils/v1.0.0";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix/v1.28.0";
+      inputs.nixpkgs.follows = "nixos";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+    pythoneda = {
+      url = "github:pythoneda/base/0.0.1a7";
+      inputs.nixos.follows = "nixos";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.poetry2nix.follows = "poetry2nix";
+    };
+    pythoneda-nix-flakes = {
+      url = "github:pythoneda/nix-flakes/0.0.1a2";
+      inputs.pythoneda.follows = "pythoneda";
+      inputs.nixos.follows = "nixos";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.poetry2nix.follows = "poetry2nix";
+    };
+    pythoneda-infrastructure-base = {
+      url = "github:pythoneda-infrastructure/base/0.0.1a5";
+      inputs.pythoneda.follows = "pythoneda";
+      inputs.nixos.follows = "nixos";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.poetry2nix.follows = "poetry2nix";
+    };
+  };
+  outputs = inputs:
+    with inputs;
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixos { inherit system; };
+        python = pkgs.python3;
+        pythonPackages = python.pkgs;
+        description = "Infrastructure layer of PythonEDA Nix Flakes";
+        license = pkgs.lib.licenses.gpl3;
+        homepage = "https://github.com/pythoneda-infrastructure/nix-flakes";
+        maintainers = with pkgs.lib.maintainers; [ ];
+      in rec {
+        packages = {
+          pythoneda-infrastructure-nix-flakes =
+            pythonPackages.buildPythonPackage rec {
+              pname = "pythoneda-infrastructure-nix-flakes";
+              version = "0.0.1a2";
+              src = ./.;
+              format = "pyproject";
+
+              nativeBuildInputs = [ pkgs.poetry ];
+
+              propagatedBuildInputs = with pythonPackages; [
+                pythoneda-nix-flakes.packages.${system}.pythoneda-nix-flakes
+                pythoneda-infrastructure-base.packages.${system}.pythoneda-infrastructure-base
+              ];
+
+              checkInputs = with pythonPackages; [ pytest ];
+
+              pythonImportsCheck = [ ];
+
+              meta = with pkgs.lib; {
+                inherit description license homepage maintainers;
+              };
+            };
+          default = packages.pythoneda-infrastructure-nix-flakes;
+          meta = with lib; {
+            inherit description license homepage maintainers;
+          };
+        };
+        defaultPackage = packages.default;
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs.python3Packages; [ packages.default ];
+        };
+        shell = flake-utils.lib.mkShell {
+          packages = system: [ self.packages.${system}.default ];
+        };
+      });
+}
